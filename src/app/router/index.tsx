23a0bfb41a32b8bdb69d4af5403030484a677fa2
DimaDevelopment <dima.development@fakel.com.ua>
2023-11-06 15:41:19 +0200

    resolve merge confilcts

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -38,221 +38,224 @@ import {
 } from "@/modules/vendors";
 import { CreateTaskPage, UpdateTaskPage, TasksListPage } from "@/modules/tasks";
 
-export const router = createBrowserRouter([
-  {
-    path: AppRoutes.LOGIN,
-    element: <LoginPage />,
-  },
-  {
-    path: AppRoutes.CREATE_PROFILE,
-    element: <CreateProfilePage />,
-  },
-  {
-    path: AppRoutes.ROOT,
-    element: (
-      <ProtectedRoute>
-        <TasksListPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: AppRoutes.CREATE_TASK,
-    element: (
-      <ProtectedRoute>
-        <CreateTaskPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: AppRoutes.UPDATE_TASK,
-    element: (
-      <ProtectedRoute>
-        <UpdateTaskPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: AppRoutes.PAYMENTS_LIST,
-    element: (
-      <ProtectedRoute>
-        <PaymentsIndex />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: AppRoutes.CREATE_PAYMENT,
-    element: (
-      <ProtectedRoute>
-        <CreatePaymentPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: AppRoutes.UPDATE_PAYMENT,
-    element: (
-      <ProtectedRoute>
-        <UpdatePaymentPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: AppRoutes.GUESTS_LIST,
-    element: (
-      <ProtectedRoute>
-        <GuestsIndex />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: AppRoutes.CREATE_GUEST,
-    element: (
-      <ProtectedRoute>
-        <CreateGuestPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: AppRoutes.UPDATE_GUEST,
-    element: (
-      <ProtectedRoute>
-        <UpdateGuestPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: AppRoutes.PURCHASES_LIST,
-    element: (
-      <ProtectedRoute>
-        <PurchasesIndex />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: AppRoutes.CREATE_PURCHASE,
-    element: (
-      <ProtectedRoute>
-        <CreatePurchasePage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: `${AppRoutes.PURCHASES_BEST_IDEAS}/:id`,
-    element: (
-      <ProtectedRoute>
-        <PurchaseBestIdeas />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: `${AppRoutes.PURCHASES_BEST_IDEAS}`,
-    element: (
-      <ProtectedRoute>
-        <SinglePurchasePage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: `${AppRoutes.MANUAL_PURCHAES_LIST}`,
-    element: (
-      <ProtectedRoute>
-        <ManualProductList />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: `${AppRoutes.WEDDING_PROFILE}`,
-    element: (
-      <ProtectedRoute>
-        <WeddingProfileIndex />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: `${AppRoutes.REGION_SETTINGS}`,
-    element: (
-      <ProtectedRoute>
-        <RegionSettingsIndex />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: `${AppRoutes.DESIGN_SETTINGS}`,
-    element: (
-      <ProtectedRoute>
-        <DesignSettingsIndex />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: `${AppRoutes.INVITE_SEETINGS}`,
-    element: (
-      <ProtectedRoute>
-        <InviteSettingsIndex />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: `${AppRoutes.POSTS}`,
-    element: (
-      <ProtectedRoute>
-        <PostsIndex />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: `${AppRoutes.POST}`,
-    element: (
-      <ProtectedRoute>
-        <SinglePost />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: `${AppRoutes.VENDORS}`,
-    element: (
-      <ProtectedRoute>
-        <VendorsIndex />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: `${AppRoutes.SEARCH_VENDORS}`,
-    element: (
-      <ProtectedRoute>
-        <ResultSearchVendors />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: `${AppRoutes.SINGLE_VENDOR}`,
-    element: (
-      <ProtectedRoute>
-        <SingleVendor />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: `${AppRoutes.CREATE_VENDOR}`,
-    element: (
-      <ProtectedRoute>
-        <CreateVendorPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: `${AppRoutes.UPDATE_VENDOR}`,
-    element: (
-      <ProtectedRoute>
-        <UpdateVendorPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: `${AppRoutes.SET_USER_LOCATION}`,
-    element: (
-      <ProtectedRoute>
-        <SetUserLocationPage />
-      </ProtectedRoute>
-    ),
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: AppRoutes.LOGIN,
+      element: <LoginPage />,
+    },
+    {
+      path: AppRoutes.CREATE_PROFILE,
+      element: <CreateProfilePage />,
+    },
+    {
+      path: AppRoutes.ROOT,
+      element: (
+        <ProtectedRoute>
+          <TasksListPage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: AppRoutes.CREATE_TASK,
+      element: (
+        <ProtectedRoute>
+          <CreateTaskPage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: AppRoutes.UPDATE_TASK,
+      element: (
+        <ProtectedRoute>
+          <UpdateTaskPage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: AppRoutes.PAYMENTS_LIST,
+      element: (
+        <ProtectedRoute>
+          <PaymentsIndex />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: AppRoutes.CREATE_PAYMENT,
+      element: (
+        <ProtectedRoute>
+          <CreatePaymentPage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: AppRoutes.UPDATE_PAYMENT,
+      element: (
+        <ProtectedRoute>
+          <UpdatePaymentPage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: AppRoutes.GUESTS_LIST,
+      element: (
+        <ProtectedRoute>
+          <GuestsIndex />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: AppRoutes.CREATE_GUEST,
+      element: (
+        <ProtectedRoute>
+          <CreateGuestPage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: AppRoutes.UPDATE_GUEST,
+      element: (
+        <ProtectedRoute>
+          <UpdateGuestPage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: AppRoutes.PURCHASES_LIST,
+      element: (
+        <ProtectedRoute>
+          <PurchasesIndex />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: AppRoutes.CREATE_PURCHASE,
+      element: (
+        <ProtectedRoute>
+          <CreatePurchasePage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: `${AppRoutes.PURCHASES_BEST_IDEAS}/:id`,
+      element: (
+        <ProtectedRoute>
+          <PurchaseBestIdeas />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: `${AppRoutes.PURCHASES_BEST_IDEAS}`,
+      element: (
+        <ProtectedRoute>
+          <SinglePurchasePage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: `${AppRoutes.MANUAL_PURCHAES_LIST}`,
+      element: (
+        <ProtectedRoute>
+          <ManualProductList />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: `${AppRoutes.WEDDING_PROFILE}`,
+      element: (
+        <ProtectedRoute>
+          <WeddingProfileIndex />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: `${AppRoutes.REGION_SETTINGS}`,
+      element: (
+        <ProtectedRoute>
+          <RegionSettingsIndex />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: `${AppRoutes.DESIGN_SETTINGS}`,
+      element: (
+        <ProtectedRoute>
+          <DesignSettingsIndex />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: `${AppRoutes.INVITE_SEETINGS}`,
+      element: (
+        <ProtectedRoute>
+          <InviteSettingsIndex />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: `${AppRoutes.POSTS}`,
+      element: (
+        <ProtectedRoute>
+          <PostsIndex />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: `${AppRoutes.POST}`,
+      element: (
+        <ProtectedRoute>
+          <SinglePost />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: `${AppRoutes.VENDORS}`,
+      element: (
+        <ProtectedRoute>
+          <VendorsIndex />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: `${AppRoutes.SEARCH_VENDORS}`,
+      element: (
+        <ProtectedRoute>
+          <ResultSearchVendors />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: `${AppRoutes.SINGLE_VENDOR}`,
+      element: (
+        <ProtectedRoute>
+          <SingleVendor />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: `${AppRoutes.CREATE_VENDOR}`,
+      element: (
+        <ProtectedRoute>
+          <CreateVendorPage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: `${AppRoutes.UPDATE_VENDOR}`,
+      element: (
+        <ProtectedRoute>
+          <UpdateVendorPage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: `${AppRoutes.SET_USER_LOCATION}`,
+      element: (
+        <ProtectedRoute>
+          <SetUserLocationPage />
+        </ProtectedRoute>
+      ),
+    },
+  ],
+  { basename: "/planner" }
+);
